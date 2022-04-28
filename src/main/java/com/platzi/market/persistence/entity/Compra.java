6cafb09b99bcd944134513a3a9124adc9a9af787
JMaycol-Valencia <maycolvalencia50@gmail.com>
2022-04-27 20:05:19 -0400

    segundo commit

---
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -0,0 +1,84 @@
+package com.platzi.market.persistence.entity;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer IdCompra;
+
+
+
+    @Column(name = "Id_cliente")
+    private String IdCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "producto")
+    private List<ComprasProducto> productos;
+
+    public Integer getIdCompra() {
+        return IdCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        IdCompra = idCompra;
+    }
+
+    public String getIdCliente() {
+        return IdCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        IdCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
